50d5680abd3bedb6e5b327912783ce30809ad1b1
johnkischel <johnkischel@gmail.com>
2022-02-07 16:26:08 +0100

    changed base images

--- coffeedb/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from main.models import *
-
-# Create your views here.
-def index(request):
-    context={}
-    context['product'] = Product.objects.all()
-    return render(request,'index.html',context)
--- dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3
-ENV PYTHONBUFFERED=1
-ENV PYTHONWRITEBYTECODE=1
-LABEL maintainer johnkischel@gmail.com
-WORKDIR /code
-COPY requirements.txt /code/
-RUN pip install -r requirements.txt
-COPY . /code/
